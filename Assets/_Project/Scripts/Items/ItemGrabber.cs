b04cd5dbc7a8a5c43891bc67672f2db86046902a
ElasticSea <info@elasticsea.com>
2021-06-02 17:18:24 +0200

    Hooking up item metadata UI

--- Assets/_Project/Scripts/Items/ItemGrabber.cs
+++ Assets/_Project/Scripts/Items/ItemGrabber.cs
@@ -1,27 +1,28 @@
 using ElasticSea.Framework.Util.PropertyDrawers;
 using Items.Ui;
+using Rigs;
 using UnityEngine;
 
 namespace Items
 {
     public class ItemGrabber : MonoBehaviour
     {
-        // [SerializeField] private OVRGrabber grabber;
+        [SerializeField] private Hand grabber;
         
         [CustomObjectPicker(typeof(IItemMetadataUi))]
         [SerializeField] private Component itemMetadataUi;
         
         private void Update()
         {
-            // var grabbedObject = grabber.grabbedObject;
-            // if (grabbedObject && grabbedObject.GetComponent<ShoppingItem>())
-            // {
-            //     ((IItemMetadataUi)itemMetadataUi).Item = grabbedObject.GetComponent<ShoppingItem>();
-            // }
-            // else
-            // {
-            //     ((IItemMetadataUi)itemMetadataUi).Item = null;
-            // }
+            var grabbedObject = grabber.Grabbed;
+            if (grabbedObject && grabbedObject.GetComponent<ShoppingItem>())
+            {
+                ((IItemMetadataUi)itemMetadataUi).Item = grabbedObject.GetComponent<ShoppingItem>();
+            }
+            else
+            {
+                ((IItemMetadataUi)itemMetadataUi).Item = null;
+            }
         }
     }
 }
